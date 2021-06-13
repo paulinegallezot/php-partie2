b86788eed455fb9caad9b3bca016060c56785209
paulinegallezot <paulinegallezot@gmail.com>
2021-06-13 20:16:34 +0200

    commit

--- ex1/index.php
+++ ex1/index.php
@@ -18,7 +18,7 @@ if($age >= 18){
 <body>
 Exercice 1 Devoir
 Créer une variable age et l'initialiser avec une valeur.  
-Si l'âge est supérieur ou égale à 18, afficher Vous êtes majeur. Dans le cas contraire, afficher Vous êtes mineur.
+Si l'âge est supérieur ou égale à 18, afficher "Vous êtes majeur. Dans le cas contraire, afficher Vous êtes mineur.
 <br>
     <?php
             echo $result;
